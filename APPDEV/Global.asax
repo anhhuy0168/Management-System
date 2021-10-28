9a926b5ba8ef60af731779e7dd24e03fd7be69f5
huy nguyen anh <anhhuy123zxc123@gmail.com>
2021-10-28 19:28:16 +0700

    Add project files.

---
+++ APPDEV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APPDEV.MvcApplication" Language="C#" %>
---
+++ APPDEV/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APPDEV.Startup))]
+namespace APPDEV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
