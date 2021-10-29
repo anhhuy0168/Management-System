f049de3b711e0d4608505f1188d9f203011632a8
huy nguyen anh <anhhuy123zxc123@gmail.com>
2021-10-29 09:36:50 +0700

    CreateTraineeTable

--- APPDEV/Models/ApplicationDbContext.cs
+++ APPDEV/Models/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace APPDEV.Models
         public DbSet<Course> Courses { get; set; }
         public DbSet<Staff> Staffs { get; set; }
         public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<Trainee> Trainees { get; set; }
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
---
+++ APPDEV/Models/Trainee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace APPDEV.Models
+{
+    public class Trainee
+    {
+        [Key]
+        [ForeignKey("User")]
+        //khoa ngoai vs bang user
+        public string TraineeId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        //for validation
+        [Required]
+
+        public string FullName { get; set; }
+
+        [Required]
+
+        public int Age { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+
+        public string Education { get; set; }
+    }
+}
