bf317d1589b9df188e35220357257b8c97c1c78d
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2021-10-29 20:14:53 +0700

    Trainee  view profile

---
+++ APPDEV/Controllers/TraineesController.cs
@@ -0,0 +1,28 @@
+﻿using APPDEV.Models;
+using APPDEV.Utils;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APPDEV.Controllers
+{
+    public class TraineesController : Controller
+    {
+        private ApplicationDbContext _context;
+        public TraineesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Trainees
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var traineeInDb = _context.Trainees
+                .SingleOrDefault(t => t.TraineeId == userId);
+            return View(traineeInDb);
+        }
+    }
+}
