29278d3cbb38512901994878f887eca2ef1d2e64
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2021-10-29 11:46:24 +0700

    Commit test

---
+++ APPDEV/Controllers/AssignTrainerToCousesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APPDEV.Controllers
+{
+    public class AssignTrainerToCousesController : Controller
+    {
+        // GET: AssignTrainerToCouses
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
