049e33dba97a131d52249363bed1742b93ad7820
huy nguyen anh <anhhuy123zxc123@gmail.com>
2021-10-29 09:22:21 +0700

    Create Role and AdminsController with indexstaff and index trainer

---
+++ APPDEV/Controllers/AdminsController.cs
@@ -0,0 +1,72 @@
+﻿using APPDEV.Models;
+using APPDEV.Utils;
+using APPDEV.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APPDEV.Controllers
+{
+    public class AdminsController : Controller
+    {
+        //tao ket noi
+        private ApplicationDbContext _context;
+
+        private ApplicationUserManager _userManager;
+
+        public AdminsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public AdminsController(ApplicationUserManager userManager)
+        {
+            _context = new ApplicationDbContext();
+            UserManager = userManager;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        public ActionResult IndexStaff()
+        {
+            var staff = _context.Staffs.ToList();
+            var user = _context.Users.ToList();
+
+            return View(staff);
+        }
+
+       
+
+        /// <summary>
+        /// FOR TRAINER
+        /// </summary>
+
+        [Authorize(Roles = "admin ")]
+        [HttpGet]
+        public ActionResult IndexTrainer()
+        {
+            var trainer = _context.Trainers.ToList();
+            var user = _context.Users.ToList();
+
+            return View(trainer);
+        }
+
+       
+    }
+}
---
+++ APPDEV/Utils/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APPDEV.Utils
+{
+    public class Role
+    {
+        public const string Admin = "admin";
+        public const string Staff = "staff";
+        public const string Trainer = "trainer";
+        public const string Trainee = "trainee";
+    }
+}
