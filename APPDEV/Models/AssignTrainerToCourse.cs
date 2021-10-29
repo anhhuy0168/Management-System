b046df7e597cb6bb0e3358e6331ea9d6590f2a57
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2021-10-29 16:08:00 +0700

    Create model assigntrainertocouse

---
+++ APPDEV/Models/AssignTrainerToCourse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace APPDEV.Models
+{
+    public class AssignTrainerToCourse
+    {
+        [Key, Column(Order = 1)]
+        [ForeignKey("Trainer")]
+        public string TrainerId { get; set; }
+
+        [Key, Column(Order = 2)]
+        [ForeignKey("Course")]
+        public int CourseId { get; set; }
+
+        public Trainer Trainer { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
