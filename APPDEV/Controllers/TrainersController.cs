a40d84c65acb3abd53ce786e047700035cf55adc
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2021-11-01 19:20:07 +0700

    Index Trainer view profile

---
+++ APPDEV/Controllers/TrainersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using APPDEV.Models;
+using APPDEV.Utils;
+using APPDEV.ViewModels;
+
+namespace APPDEV.Controllers
+{
+    [Authorize(Roles = Role.Trainer)]
+    public class TrainersController : Controller
+    {
+        private ApplicationDbContext _context;
+        public TrainersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Trainers
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var trainerInDb = _context.Trainers.SingleOrDefault(t => t.TrainerId == userId);
+            return View(trainerInDb);
+        }
+
+    }
+}
